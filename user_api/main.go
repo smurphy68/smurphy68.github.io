59183be85fae8892e1ddfa5edf964a6d5801ce62
smurphy68 <smurphy68@live.co.uk>
2025-09-23 00:14:54 +0100

    stash - postgres container added

--- user_api/main.go
+++ user_api/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/smurphy68/user_api/consts"
 	"github.com/smurphy68/user_api/routes"
@@ -12,5 +16,26 @@ func main() {
 	routes.RegisterRoutes(r)
 	services.TryStartTopic(consts.KAFKAPORT, consts.USERSTOPIC)
 
+	conn := connectToDB()
+	defer conn.Close()
+
 	r.Run(":8080")
+}
+
+func connectToDB() *sql.DB {
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	connectionString := fmt.Sprintf(
+		"postgres://%s:%s@postgres:5432/%s?sslmode=disable",
+		user,
+		password,
+		dbName,
+	)
+
+	conn, e := sql.Open("postgres", connectionString)
+	if e != nil {
+		services.HandleError(e)
+	}
+	return conn
 }
